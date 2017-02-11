e41e6605b3fa7aa13af16916399f8b0351addf5b
Michael Jones <michaelscottjones0@gmail.com>
2017-02-11 13:23:42 -0500

    App Skeleton
    Activities made but empty

---
+++ EPA/app/src/main/java/com/example/michael/epa/HelpPage.java
@@ -0,0 +1,13 @@
+package com.example.michael.epa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_page);
+    }
+}
---
+++ EPA/app/src/main/java/com/example/michael/epa/SelectionScreen.java
@@ -0,0 +1,13 @@
+package com.example.michael.epa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen);
+    }
+}
